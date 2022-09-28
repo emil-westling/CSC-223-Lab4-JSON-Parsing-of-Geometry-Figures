c28d43adbcdbfe7a41cf6e164cb09177e43307ee
Miles Dame <milesedame@gmail.com>
2022-09-28 15:59:18 -0400

    Add getPoints method

--- src/input/parser/JSONParser.java
+++ src/input/parser/JSONParser.java
@@ -39,12 +39,40 @@ public class JSONParser
 		// Parsing is accomplished via the JSONTokenizer class.
 		JSONTokener tokenizer = new JSONTokener(str);
 		JSONObject  JSONroot = (JSONObject)tokenizer.nextValue();
+		
+		getPoints(JSONroot);
+		
+		return null;
 
         // TODO: Build the whole AST, check for return class object, and return the root
 	}
-
     // TODO: implement supporting functionality
+	
+	private PointNodeDatabase getPoints(JSONObject obj) {
+		JSONArray points = obj.getJSONArray("Points");
+		System.out.println(points.toString());
+		PointNodeDatabase pointDB = new PointNodeDatabase();
+		
+		
+		for(Object o : points) {
+			String name = ((JSONObject) o).getString("name");
+			Double x = ((JSONObject) o).getDouble("x");
+			Double y = ((JSONObject) o).getDouble("y");
+			PointNode node = new PointNode(name, x, y);
+			
+			pointDB.put(node);
+		}
+		return pointDB;
+	}
+	
+
+	public static void main(String[] args ) {
+		JSONParser p = new JSONParser();
+		String figureStr = utilities.io.FileUtilities.readFileFilterComments("/CSC-223-Lab4/collinear_line_segments.json");
+		ComponentNode n = p.parse(figureStr);
+	}
 	
 	
 
 }
+
